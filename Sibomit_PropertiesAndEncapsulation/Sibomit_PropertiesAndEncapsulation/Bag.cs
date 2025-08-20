1af102a29dfd9ec7b22183bddd1723022614058f
ACT-STUDENT <ACT-STUDENT@LAB3-PC06>
2025-08-20 14:31:56 +0800

    Program for Basic Encapsulation

---
+++ Sibomit_PropertiesAndEncapsulation/Sibomit_PropertiesAndEncapsulation/Bag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_PropertiesAndEncapsulation
+{
+    internal class Bag
+    {
+        //Declare the properties of the Bag class
+        private string brand;
+        private string color;
+
+        //Set the properties for the Bag class fields
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        //Method to display Bag information
+        public void DisplayBagInfo()
+        {
+            Console.WriteLine($"Brand: {brand}\nColor: {color}");
+        }
+    }
+}
---
+++ Sibomit_PropertiesAndEncapsulation/Sibomit_PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        //Name: Monaliza Jane Sibomit
+        //Section: IT306
+        //Exercise: Basics on Encapsulation
+
+        static void Main(string[] args)
+        {
+            //Instantiate the Bag class
+            Bag bag1 = new Bag();
+
+            //Set the properties for the Bag class fields
+            bag1.Brand = "Prada";
+            bag1.Color = "Black";
+
+            //Display the Bag information
+            bag1.DisplayBagInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
