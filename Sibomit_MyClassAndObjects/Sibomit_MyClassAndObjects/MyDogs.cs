a811adbe9ac398bcc80dd8df395174ca82b9b909
ACT-STUDENT <ACT-STUDENT@LAB3-PC06>
2025-08-29 14:28:41 +0800

    Program on My Class and Objects

---
+++ Sibomit_MyClassAndObjects/Sibomit_MyClassAndObjects/MyDogs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_MyClassAndObjects
+{
+    internal class MyDogs
+    {
+        //fields or attributes
+        public string name, breed;
+        public int age;
+
+        //method to display
+        public void DisplayInfo()
+        {
+            Console.WriteLine("-" + name + ", " + age + " months old, " + breed);
+        }
+    }
+}
---
+++ Sibomit_MyClassAndObjects/Sibomit_MyClassAndObjects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_MyClassAndObjects
+{
+    internal class Program
+    {
+        //Name: Monaliza Jane Sibomit
+        //Section: IT306
+
+        //Exercise: My Class and Objects
+        static void Main(string[] args)
+        {
+            //instantiation of an object
+            MyDogs myDog1 = new MyDogs();
+            MyDogs myDog2 = new MyDogs();
+
+            //initialization of fields
+            myDog1.name = "Yushi";
+            myDog1.age = 4;
+            myDog1.breed = "Spitz";
+
+            myDog2.name = "Ryo";
+            myDog2.age = 5;
+            myDog2.breed = "Shih Tzu";
+
+            //print the title once
+            Console.WriteLine("Here are the list of my dogs:\n");
+
+            //call the method
+            myDog1.DisplayInfo();
+            myDog2.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
