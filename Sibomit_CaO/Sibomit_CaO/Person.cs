df42338cabba08178f1d0ec0505a2f3782fb49e0
ACT-STUDENT <ACT-STUDENT@LAB3-PC06>
2025-08-29 14:25:12 +0800

    Program on Creating Class and Objects

---
+++ Sibomit_CaO/Sibomit_CaO/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_CaO
+{
+    internal class Person
+    {
+        //class members
+        public string name;
+        public int age;
+        public string gender;
+
+        //method to display
+        public void Greet()
+        {
+            Console.WriteLine("Hello everyone, my name is " + name + ", I'm " + age + " and I'm a " + gender + ".");
+        }
+    }
+}
---
+++ Sibomit_CaO/Sibomit_CaO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_CaO
+{
+    internal class Program
+    {
+        //Name: Monaliza Jane Sibomit
+        //Section: IT306
+
+        //EXER_01: Creating a Class
+        static void Main(string[] args)
+        {
+            //instantiation of an object
+            Person person1 = new Person();
+
+            ////initialization of fields
+            person1.name = "Monaliza Jane";
+            person1.age = 22;
+            person1.gender = "bisexual";
+
+            //call the method
+            person1.Greet();
+
+            Console.ReadKey();
+        }
+    }
+}
