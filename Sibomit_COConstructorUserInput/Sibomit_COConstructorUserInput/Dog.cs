48f682592b21644880475cb1e360ec981e326495
ACT-STUDENT <ACT-STUDENT@LAB3-PC06>
2025-08-29 14:32:07 +0800

    Program on Constructor with User Input

---
+++ Sibomit_COConstructorUserInput/Sibomit_COConstructorUserInput/Dog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_COConstructorUserInput
+{
+    internal class Dog
+    {
+        //fields or attributes
+        public string name;
+        public int age;
+        public string breed;
+
+        public Dog(string dName, int dAge, string dBreed) //parameterized constructor
+        {
+            name = dName;
+            age = dAge;
+            breed = dBreed;
+        }
+
+        //method to display dog details
+        public void DisplayDogDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("HERE ARE THE DOG DETAILS:\n");
+            Console.WriteLine("Dog Name: \t" + name);
+            Console.WriteLine("Age: \t\t" + age);
+            Console.WriteLine("Breed: \t\t" + breed);
+        }
+    }
+}
---
+++ Sibomit_COConstructorUserInput/Sibomit_COConstructorUserInput/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_COConstructorUserInput
+{
+    internal class Program
+    {
+        //Name: Monaliza Jane Sibomi
+        //Section: IT306
+
+        //User Input with Constructor
+        static void Main(string[] args)
+        {
+        Main:
+            Console.Clear();
+
+            //Ask the user to input the details
+            Console.WriteLine("This program will ask about your dog's information\n");
+
+            Console.Write("Enter the name of your dog: \t\t");
+            string dName = Console.ReadLine();
+
+            Console.Write("Enter the age of your dog (months): \t");
+            int dAge = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the breed of your dog: \t\t");
+            string dBreed = Console.ReadLine();
+
+            //instantiation of an object
+            Dog dog1 = new Dog(dName, dAge, dBreed);
+
+            //call the method to display
+            dog1.DisplayDogDetails();
+
+            //input another dog details
+            Console.Write("\nDo you want to enter the dog's information again (y/n)? ");
+            char answer = Convert.ToChar(Console.ReadLine());
+
+            if (answer == 'y')
+            {
+                goto Main;
+            }
+            else if (answer == 'n')
+            {
+                return;
+            }
+        }
+    }
+}
