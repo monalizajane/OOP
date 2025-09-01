de7a111a75c15fc3b5260e016385353f7bdad938
ACT-STUDENT <ACT-STUDENT@LAB3-PC24>
2025-09-01 14:36:55 +0800

    Program on Simple Inheritance

---
+++ Sibomit_SimpleInheritance/Sibomit_SimpleInheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_SimpleInheritance
+{
+    internal class Program
+    {
+        //Name: Monaliza Jane Sibomit
+        //Section: IT306
+        //Exercise: Simple Inheritance
+        static void Main(string[] args)
+        {
+            //Create an instance of the Regular Student class
+            RegularStudent regularStudent1 = new RegularStudent
+            {
+                Name = "Beatrice Joy Carvellida",
+                Program = "BSCPE",
+                Section = "CPE301"
+            };
+            Console.WriteLine("\n***** REGULAR STUDENT *****\n");
+            regularStudent1.BasicInfo();    //Call inherited method to display student information
+            regularStudent1.SectionEnrolled();  //Call method to display regular student information
+
+            //Create an instance of the Irregular Student class
+            IrregularStudent irregularStudent1 = new IrregularStudent
+            {
+                Name = "Nicole Ann Ubod",
+                Program = "BSTM",
+                UnitsEnrolled = 15
+            };
+            Console.WriteLine("\n***** IRREGULAR STUDENT *****\n");
+            irregularStudent1.BasicInfo();  //Call inherited method to display student information
+            irregularStudent1.EnrolledSemUnits();   //Call method to display irregular student information
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sibomit_SimpleInheritance/Sibomit_SimpleInheritance/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_SimpleInheritance
+{
+    internal class Student
+    {
+        //Properties fr the Student Class
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        //Method to display student information
+        public void BasicInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nProgram: {Program}");
+        }
+    }
+
+    class RegularStudent : Student
+    {
+        //Additional property for the Regular Student class
+        public string Section { get; set; }
+
+        //Method to display regular student information
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"Section: {Section}");
+        }
+    }
+
+    class IrregularStudent : Student
+    {
+        //Additinal property for the Irregular Student class
+        public int UnitsEnrolled { get; set; }
+
+        //Method to display irregular student information
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine($"Units Enrolled: {UnitsEnrolled}");
+        }
+    }
+}
