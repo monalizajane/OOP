7fca247c949d1be97223c3b24253e1f77b52869c
ACT-STUDENT <ACT-STUDENT@LAB3-PC06>
2025-09-05 14:10:44 +0800

    Program on Inheritance with Constructor

---
+++ Sibomit_InheritanceWithConstructor/Sibomit_InheritanceWithConstructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_InheritanceWithConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an instance of the regular class student
+            RegularStudent regularStudent1 = new RegularStudent("Beatrice Joy Carvellida", "BSCPE", "CPE301");
+            Console.WriteLine("\n***** REGULAR STUDENT *****\n");
+            regularStudent1.SectionEnrolled();  //call method to display regular student info
+
+            //create an instance of the irregular class student
+            IrregularStudent irregularStudent1 = new IrregularStudent("Nicole Ann Ubod", "BSTM", 15);
+            Console.WriteLine("\n***** IRREGULAR STUDENT *****\n");
+            irregularStudent1.EnrolledSemUnits();   //call method to display irregular student info
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sibomit_InheritanceWithConstructor/Sibomit_InheritanceWithConstructor/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_InheritanceWithConstructor
+{
+    internal class Student  //base class
+    {
+        //declare the fields for student class
+        public string name;
+        public string program;
+
+        //constructor
+        public Student(string studentName, string studentProgram)
+        {
+            name = studentName;
+            program = studentProgram;
+        }
+
+        //method to display studnet info
+        public void BasicInfo()
+        {
+            Console.WriteLine($"Name: {name}\nProgram: {program}");
+        }
+    }
+
+    class RegularStudent : Student  //derived class
+    {
+        //additional field
+        public string section;
+
+        //constructor
+        public RegularStudent(string studentName, string studentProgram, string studentSection) : base(studentName, studentProgram)
+        {
+            section = studentSection;
+        }
+
+        //method to display regular student info
+        public void SectionEnrolled()
+        {
+            BasicInfo();    //call base class method to display
+            Console.WriteLine($"Section: {section}");
+        }
+    }
+
+    class IrregularStudent : Student    //derived class
+    {
+        //additional field
+        public int unitsEnrolled;
+
+        //constructor
+        public IrregularStudent(string studentName, string studentProgram, int studentUnits) : base(studentName, studentProgram)
+        {
+            unitsEnrolled = studentUnits;
+        }
+
+        //method to display irregular student info
+        public void EnrolledSemUnits()
+        {
+            BasicInfo();   //call base class method to display
+            Console.WriteLine($"Units Enrolled: {unitsEnrolled}");
+        }
+    }
+}
