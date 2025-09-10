f02aac10b88366e21ec872dd934885d79b007931
ACT-STUDENT <ACT-STUDENT@LAB3-PC06>
2025-09-10 14:56:40 +0800

    Program on Inheritance with User Input

---
+++ Sibomit_InheritanceUserInput/Sibomit_InheritanceUserInput/Bag.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_InheritanceUserInput
+{
+    internal class Bag
+    {
+        //declare the fields
+        public string brand;
+        public double price;
+
+        //constructor
+        public Bag(string bagBrand, double bagPrice)
+        {
+            brand = bagBrand;
+            price = bagPrice;
+        }
+
+        //method to display
+        public void BagDetails()
+        {
+            Console.WriteLine($"Brand: {brand}\nPrice: ${price}");
+        }
+    }
+
+    class Backpack : Bag
+    {
+        //additional fields
+        public string color;
+        public char laptopComp;
+
+        //constructor
+        public Backpack(string bagBrand, double bagPrice, string bagColor, char bagLaptopComp) : base(bagBrand, bagPrice)
+        {
+            color = bagColor;
+            laptopComp = bagLaptopComp;
+        }
+
+        //method to display
+        public void BackpackDetails()
+        {
+            Console.WriteLine($"Color: {color}\nHas a Laptop Compartment (y/n): {laptopComp}");
+        }
+    }
+
+    class Handbag : Bag
+    {
+        //additional fields
+        public string material;
+        public char shoulderStrap;
+
+        //constructor
+        public Handbag(string bagBrand, double bagPrice, string bagMaterial, char bagShoulderStrap) : base(bagBrand, bagPrice)
+        {
+            material = bagMaterial;
+            shoulderStrap = bagShoulderStrap;
+        }
+
+        //method to display
+        public void HandbagDetails()
+        {
+            Console.WriteLine($"Material: {material}\nHas a Shoulder Strap (y/n): {shoulderStrap}");
+        }
+    }
+}
---
+++ Sibomit_InheritanceUserInput/Sibomit_InheritanceUserInput/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_InheritanceUserInput
+{
+    internal class Program
+    {
+        //Name: Monaliza Jane Sibomit
+        //Section: IT306
+        //Exercise: Inheritance with User Input
+        static void Main(string[] args)
+        {
+        Main:
+            //user will select which derived class to input
+            Console.WriteLine("Choose which bag would you like to provide information for (A/B): ");
+            Console.WriteLine("A. Backpack\nB.Handbag");
+            char answer = char.Parse(Console.ReadLine());
+
+            if (answer == 'A')
+            {
+                Console.Clear();
+
+                //user will input backpack details
+                Console.WriteLine("Enter the Backpack Details:");
+                Console.Write("Brand: ");
+                string brand = Console.ReadLine();
+                Console.Write("Price: $");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Color: ");
+                string color = Console.ReadLine();
+                Console.Write("Has a Laptop Compartment (Y/N): ");
+                char laptopComp = char.Parse(Console.ReadLine());
+
+                //create an instance
+                Backpack myBackpack = new Backpack(brand, price, color, laptopComp);
+
+                //call the method to display
+                Console.Clear();
+                Console.WriteLine("***BACKPACK DETAILS***");
+                myBackpack.BagDetails();
+                myBackpack.BackpackDetails();
+            }
+
+            else if (answer == 'B')
+            {
+                Console.Clear();
+
+                //user will input handbag details
+                Console.WriteLine("Enter the Handbag Details:");
+                Console.Write("Brand: ");
+                string brand = Console.ReadLine();
+                Console.Write("Price: $");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Color: ");
+                string material = Console.ReadLine();
+                Console.Write("Has a Shoulder Strap (Y/N): ");
+                char shoulderStrap = char.Parse(Console.ReadLine());
+
+                //create an instance
+                Handbag myHandbag = new Handbag(brand, price, material, shoulderStrap);
+
+                //call the method to display
+                Console.Clear();
+                Console.WriteLine("***HANDBAG DETAILS***");
+                myHandbag.BagDetails();
+                myHandbag.HandbagDetails();
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
