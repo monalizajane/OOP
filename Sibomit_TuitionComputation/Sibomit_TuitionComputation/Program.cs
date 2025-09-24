b88fa7b06b6fde76c5034ba9641597d72c8efb0f
ACT-STUDENT <ACT-STUDENT@LAB3-PC06>
2025-09-24 14:26:13 +0800

    Program on Tuition Computation

---
+++ Sibomit_TuitionComputation/Sibomit_TuitionComputation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_TuitionComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instance of Student
+            Student student1 = new Student
+            {
+                BaseTuition = 18000,
+                Miscellaneous = 9000
+            };
+
+            //Call method to display
+            Console.WriteLine("Regular Student");
+            student1.TotalTuitionFee();
+
+            //Instance of Irregular
+            Irregular student2 = new Irregular
+            {
+                BaseTuition = 18000,
+                UnitsEnrolled = 15,
+                RatePerUnit = 2000,
+                Miscellaneous = 9000
+            };
+
+            //Call method to display
+            Console.WriteLine("\nIrregular Student");
+            student2.TotalTuitionFee();
+
+            //Instance of Working
+            Working student3 = new Working
+            {
+                BaseTuition = 18000,
+                Miscellaneous = 9000,
+                Discount = 50
+            };
+
+            //Call method to display
+            Console.WriteLine("\nWorking Student");
+            student3.TotalTuitionFee();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sibomit_TuitionComputation/Sibomit_TuitionComputation/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_TuitionComputation
+{
+    internal class Student
+    {
+        //Properties
+        public double BaseTuition { get; set; }
+        public double Miscellaneous { get; set; }
+
+        //Compute Total Tuition
+        public virtual double ComputeTuitionFee
+        {
+            get { return BaseTuition + Miscellaneous; }
+        }
+
+        //Method to display
+        public virtual void TotalTuitionFee()
+        {
+            Console.WriteLine($"Tuition Fee: {BaseTuition}\nMiscellaneous Fee: {Miscellaneous}\nTotal Tuition Fee: {ComputeTuitionFee}" );
+        }
+    }
+
+    class Irregular : Student
+    {
+        //Additional properties
+        public double UnitsEnrolled { get; set; }
+        public double RatePerUnit { get; set; }
+
+        //Override ComputeTuitionFee
+        public override double ComputeTuitionFee
+        {
+            get { return (UnitsEnrolled * RatePerUnit) + BaseTuition + Miscellaneous; }
+        }
+
+        //Override method to display
+        public override void TotalTuitionFee()
+        {
+            Console.WriteLine($"Units Enrolled: {UnitsEnrolled}\nRate per Unit: {RatePerUnit}\nMiscellaneous Fee: {Miscellaneous}\nTotal Tuition Fee: {ComputeTuitionFee}");
+        }
+    }
+
+    class Working : Student
+    {
+        //Additional property
+        public double Discount { get; set; }
+
+        //Override ComputeTuitionFee
+        public override double ComputeTuitionFee
+        {
+            get { return (BaseTuition + Miscellaneous) - (Discount / 100); }
+        }
+
+        //Override method to display
+        public override void TotalTuitionFee()
+        {
+            Console.WriteLine($"Tuition Fee: {BaseTuition}\nMiscellaneous Fee: {Miscellaneous}\nDiscount: {Discount}%\nTotal Tuition Fee: {ComputeTuitionFee}");
+        }
+    }
+}
