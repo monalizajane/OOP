da5882ba1affe10c97ca25c0a7b8e70f6bae5539
sibom <sibom@MONALESA>
2025-10-15 13:44:49 +0800

    Program on Polymorphism

---
+++ Sibomit_Polymorphism/Sibomit_Polymorphism/Bag.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_Polymorphism
+{
+    class Bag
+    {
+        public string Brand { get; set; }
+        public double Price { get; set; }
+
+        //Virtual method for the Bag class
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}\nPrice: ₱{Price}");
+        }
+    }
+
+    //Derived class 1
+    class Backpack : Bag
+    {
+        public string Color { get; set; }
+
+        //Override method for the Backpack class
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("===== Backpack Information =====");
+            Console.WriteLine($"Brand: {Brand}\nPrice: ₱{Price}\nColor: {Color}\n");
+        }
+    }
+
+    //Derived class 2
+    class Handbag : Bag
+    {
+        public string Material { get; set; }
+
+        //Override method for the Handbag class
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("===== Handbag Information =====");
+            Console.WriteLine($"Brand: {Brand}\nPrice: ₱{Price}\nMaterial: {Material}\n");
+        }
+    }
+}
---
+++ Sibomit_Polymorphism/Sibomit_Polymorphism/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating instances of derived classes
+            Bag myBackpack = new Backpack
+            {
+                Brand = "Nike",
+                Price = 2500,
+                Color = "Black"
+            };
+            Bag myHandbag = new Handbag
+            {
+                Brand = "Gucci",
+                Price = 15000,
+                Material = "Leather"
+            };
+
+            //Calling the overridden methods
+            myBackpack.DisplayInfo();
+            myHandbag.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
