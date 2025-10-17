976b987c5110e5effdb25e627c4ae415ec29c747
sibom <sibom@MONALESA>
2025-10-17 21:53:16 +0800

    Program on Abstraction Activity

---
+++ Sibomit_AbstractionActivity/Sibomit_AbstractionActivity/Bag.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_AbstractionActivity
+{
+    abstract class Bag
+    {
+        // Common Properties
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+
+        // Constructor
+        public Bag(string brand, string color, double price)
+        {
+            Brand = brand;
+            Color = color;
+            Price = price;
+        }
+
+        // Abstract Methods
+        public abstract void DisplayDetails();
+        public abstract double CalculateDiscount(double percent);
+
+        // Concrete Method
+        public void Carry()
+        {
+            Console.WriteLine($"{Brand} bag can be carried anywhere.\n");
+        }
+    }
+
+    // Derived Class 1: SchoolBag
+    class SchoolBag : Bag
+    {
+        public int NumberOfCompartments { get; set; }
+
+        public SchoolBag(string brand, string color, double price, int compartments)
+            : base(brand, color, price)
+        {
+            NumberOfCompartments = compartments;
+        }
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine("===== SCHOOL BAG DETAILS =====");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Price: ₱{Price}");
+            Console.WriteLine($"Number of Compartments: {NumberOfCompartments}");
+        }
+
+        public override double CalculateDiscount(double percent)
+        {
+            double discount = Price * (percent / 100);
+            double discountedPrice = Price - discount;
+            Console.Write($"Discounted Price: ₱{discountedPrice}\n");
+            return discountedPrice;
+        }
+    }
+
+    // Derived Class 2: TravelBag
+    class TravelBag : Bag
+    {
+        public double Capacity { get; set; } // in liters
+
+        public TravelBag(string brand, string color, double price, double capacity)
+            : base(brand, color, price)
+        {
+            Capacity = capacity;
+        }
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine("===== TRAVEL BAG DETAILS =====");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Price: ₱{Price}");
+            Console.WriteLine($"Capacity: {Capacity} liters");
+        }
+
+        public override double CalculateDiscount(double percent)
+        {
+            // Travel bags may have a smaller discount rate
+            double discount = Price * (percent / 100);
+            double discountedPrice = Price - discount;
+            Console.Write($"Discounted Price: ₱{discountedPrice}\n");
+            return discountedPrice;
+        }
+    }
+}
---
+++ Sibomit_AbstractionActivity/Sibomit_AbstractionActivity/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibomit_AbstractionActivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of SchoolBag and TravelBag
+            SchoolBag mySchoolBag = new SchoolBag("Adidas", "Blue", 1600, 5);
+            TravelBag myTravelBag = new TravelBag("Maia", "Black", 9900, 40);
+
+            // Display details
+            mySchoolBag.DisplayDetails();
+            mySchoolBag.CalculateDiscount(10);
+            mySchoolBag.Carry();
+
+            myTravelBag.DisplayDetails();
+            myTravelBag.CalculateDiscount(5);
+            myTravelBag.Carry();
+
+            Console.ReadKey();
+        }
+    }
+}
